f9bd3bc6fb48983d528e13634d293ead50ead6af
Rany Virakvuth <ranyvirakvuth@gmail.com>
2024-06-25 20:46:02 +0700

    Pullling Data API into higher order component (hov) in react js

---
+++ src/components/user-profile/user-list.component.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import withData from '../../with-data';
+
+
+const UserList = ({data, name, email}) => (
+
+            <div className='container user-list'>
+                <h1> User List </h1>
+                {
+                    data.map(user =>
+                    <div className='post' key={user.id}>
+                    <h1> {user.name} </h1>
+                    <h2> {user.email} </h2>
+                    </div>
+                    )
+                }
+            </div>
+        );
+
+export default withData(UserList, 'https://jsonplaceholder.typicode.com/users');
